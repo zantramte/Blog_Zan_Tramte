f01dddf1cf458b068088b5246fc603283c7e6d45
Žan Tramte <zan.tramte@gmail.com>
2021-02-13 22:32:45 +0100

    Adding new webssite part on my index website. I added website for updating, deleting and reading posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_085200) do
+ActiveRecord::Schema.define(version: 2021_02_13_211934) do
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
